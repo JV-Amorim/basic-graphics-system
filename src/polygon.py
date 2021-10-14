b71890480ac19ab01ed690dc79c051a95272a2a6
JV-Amorim <joaovitoramorimx@gmail.com>
2021-10-14 15:40:35 -0300

    refactor: add extra to Polygon

--- src/polygon.py
+++ src/polygon.py
@@ -1,21 +1,15 @@
 class Polygon:
   def __init__(self, points):
-    # TODO - Check if polygon is valid (if the first and last points meet).
     if len(points) < 3:
       raise ValueError('A polygon needs at least 3 points.')
+
+    if points[0] != points[-1]:
+      raise ValueError('The polygon is invalid. The first and last point must be equal.')
+      
     self.__points = points
 
   def __repr__(self):
     return 'Points = ' + str(self.__points)
-
-  def insert_point(self, point):
-    self.__points.append(point)
-
-  def remove_point(self, index):
-    if len(self.__points) - 1 < 3:
-      print('Cannot remove the point. A polygon needs at least 3 points.')
-      return
-    self.__points[index:index+1] = []
 
   def get_points(self):
     return self.__points
